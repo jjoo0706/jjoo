a37075e6f0beae72c8fefb5263517944f37c8cff
CodeSandbox <support@codesandbox.io>
2025-01-19 20:34:00 +0000

    Sandbox snapshot commit

--- main.py
+++ main.py
@@ -5,18 +5,71 @@
 # Multiplier
 # multiplier = j + (i-1) * 3
 
-def Mult3():
+# Add inputs to it so that you can adjust the output based on multiplier how many to do. 
+# 2, 2 
+# [2, 4], [6, 8]
+def Multiplier(x, y):
     nested_list = []
-    for i in range(1, 4):
+    for i in range(1, y+1):
         i_list = []
-        for j in range(1, 4):
-            mult = j + (i-1) * 3
-            num = 3 * mult
+        for j in range(1, y+1):
+            mult = j + (i-1) * x
+            num = x * mult
             i_list += [num]
         nested_list += [i_list]
     return nested_list
 
-print(Mult3())
+# print(Multiplier(2,2))
 
-# ASSIGNED 12/24/24 
-# 
+# Given a list of integers, find the unique triples that add up to the sum 
+# [1, 2, 3, 4, 5]
+# 9
+# (1, 3, 5), (2, 3, 4)
+# x + y + z = 9 
+
+def Triples(x, y):
+    nums = y
+    triples = []
+    for i in range(len(nums)):
+        for j in range(i+1, len(nums)):
+            for k in range(j+1, len(nums)):
+                if nums[i] + nums[j] + nums[k] == x:
+                    triples += [(nums[i], + nums[j], + nums[k])]
+                else:
+                    triples = triples
+    return triples
+# print(Triples(12, [1, 2, 3, 4, 5, 6, 7, 8, 9]))
+
+# Recursion 
+def factorial(n): 
+    if n == 1: 
+        return 1 
+    else: 
+        return n * factorial(n-1) 
+
+# n = 5 
+# 5 * 4 * 3 * 2 *1 
+
+def factorial_loop(n): 
+    result = 1 
+    for i in range(1, n+1): 
+        result = result * i 
+    return result 
+
+# print(factorial(5))
+# print(factorial_loop(5))
+
+# Given a list of integers, add all the integers in the list using recursion. 
+def recursive_sum(x):
+    if len(x) == 1:
+        return x[0]
+    else:
+        return x[0] + recursive_sum(x[1:])
+print(recursive_sum([1, 2, 3, 4, 5]))
+
+# ASSIGNED 1/19 
+# Using the permutation code below as a guide, do a first attempt at doing this code in a recursive way. 
+def permutation_generation(s): 
+    perm_list = [] 
+    for letter in s: 
+        for perm in perm_list: 
