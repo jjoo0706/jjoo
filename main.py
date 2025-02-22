db6df530a02b574495c35ee487a2998bcae21f2e
CodeSandbox <support@codesandbox.io>
2025-02-22 01:46:45 +0000

    Sandbox snapshot commit

--- main.py
+++ main.py
@@ -1,71 +1,82 @@
-a = ['Alice', 'red', 'apple'] 
-b = ['Bob', 'blue', 'banana'] 
-c = ['Charlie', 'blue', 'apple'] 
-d = ['David', 'blue', 'pear']
+students = [['Alice', 90], ['Bob', 87], ['Charlie', 92], ['David', 84], ['Eric', 95], ['Frank', 80]]
 
-# # There is 1 red. 
-# # There is 3 blue. 
-# # There are 2 apple.
-# # There is 1 banana. 
-# # There is 1 pear.  
+# First, create a dictionary of the students' names and grades 
 
-# def counter(x):
-# 	# The list will always be in the same order: name, color, fruit. 
-# 	counternumF = 0
-# 	counternumC = 0
-# 	fruit_counter = []
-# 	color_counter = []
-# 	fruits = ["apple", "banana", "pear"]
-# 	color = ["red", "blue"]
-# 	for i in x:
-# 		for x in i[1:]:
-# 			if x in fruits:
-# 				if x in fruit_counter:
-# 					counternumF += 1
-# 				else:
-# 					fruit_counter = [x]
-# 					counternumF = 1
-# 			elif x in color:
-# 				if x in color_counter:
-# 					counternumC += 1
-# 				else:
-# 					color_counter = [x]
-# 					counternumC = 1
-# 	for fruits in fruit_counter:
-# 		print(fruits, counternumF)
-# 	for color in color_counter:
-# 		print(color, counternumC)
-# data = [a, b, c, d]
-# print(counter(data))
+def student_dict():
+	student = {}
+	for i in students:
+		name = i[0]
+		grade = i[1]
+		student[name] = grade
+	return student
 
-# d = {'apple': [1,2,3]}
-# print(d['apple'])
-# d['apple'] = 3 
-# print(d['apple'])
+sd = student_dict() 
 
-fruit = {} 
-color = {} 
-for l in [a, b, c, d]: 
-	for ind in range(len(l)): 
-		if ind == 1: 
-			current_color = l[ind]
-			if current_color in color: 
-				color[current_color] += 1 
-			else: 
-				color[current_color] = 1 
-		elif ind == 2: 
-			current_fruit = l[ind]
-			if current_fruit in fruit: 
-				fruit[current_fruit] += 1 
-			else: 
-				fruit[current_fruit] = 1 
-print(fruit)
-print(color)
+# Create a function that takes in a student's name and asks for input for the updated grade
+# If the student hasn't been added to the dictionary yet, make sure the function can add them and their grade to the dictionary  
 
-for i in fruit: 
-	print("There are", fruit[i], i)
-for i in color: 
-	print("There are", color[i], i)
+def grade_update(x, name):
+	if name in x:
+		new_grade = int(input("Enter the new grade: "))
+		x[name] = new_grade
+		print("Grade updated. The student's grade is now " + str(new_grade))
+	else:
+		new_grade = int(input("That student is not in the gradebook. Enter their grade to add them: "))
+		x[name] = new_grade
+		print("Student added!. The student's grade is now " + str(new_grade))
+	
+# print(sd)
+# print(grade_update(sd, 'Daniel'))
+# print(sd)
 
-# I'll write up a more detailed description of dictionaries. 
-# Based off of the write up, I want you to try to do some easy exercises on dictionaries, which I'll include as homework. 
+# Write a function that counts the number of occurrences of each word in a string. 
+
+text = "apple banana apple orange banana apple orange orange"
+
+def word(x):
+	counter = {}
+	word = ""
+	for i in x:
+		if i != " ":
+			word += i
+		else:
+			if word in counter:
+				counter[word] += 1
+			else:
+				counter[word] = 1
+			word = ""
+	if word in counter:
+		counter[word] += 1
+	else:
+		counter[word] = 1
+	return counter
+
+
+f = open('sample.txt', 'r')
+# print(f.read())
+
+# Using this, keep a counter of the frequency of words. 
+for line in f: 
+	print(line)
+
+def word_frequency():
+	f = open('sample.txt', 'r')
+	counter = {}
+	word = ""
+	for i in f:
+		for j in i:
+			if j != " " and j != "\n":
+				word += j
+			else:
+				if word in counter:
+					counter[word] += 1
+				else:
+					counter[word] = 1
+				word = ""
+	if word in counter:
+		counter[word] += 1
+	else:
+		counter[word] = 1
+	return counter
+
+print(word_frequency())
