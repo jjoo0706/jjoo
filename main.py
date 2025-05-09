72cd4e99887b97197c6ee2b61ba93dcf5dabd1a3
CodeSandbox <support@codesandbox.io>
2025-05-09 22:25:12 +0000

    Sandbox snapshot commit

--- main.py
+++ main.py
@@ -4,3 +4,36 @@
 # Write a method that will move the date the the day before. 
 # Write a method that will move the date by a given input. 
 
+class date:
+    def __init__(self, month, day, year):
+        self.month = month
+        self.day = day
+        self.year = year
+
+    def is_leap_year(self):
+        if self.year % 4 == 0 and (self.year % 100 != 0 or self.year % 400 != 0):
+            return True
+
+    def days_in_month(self, month, year):
+        if month is None:
+            month = self.month
+        if year is None:
+            year = self.year
+        if month == 2:
+            if self.is_leap_year():
+                return 29
+            else:
+                return 28
+        elif month in [4, 6, 9, 11]:
+            return 30
+        else:
+            return 31
+
+    def advance_day(self):
+        self.day += 1
+        if self.day > self.days_in_month():
+            self.day = 1
+            self.month += 1
+            if self.month > 12:
+                self.month = 1
+                self.day += 1
