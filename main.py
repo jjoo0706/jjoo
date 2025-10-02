80dfd6752e487b56086c2a70f2e2e1fcc7b8bf9d
CodeSandbox <support@codesandbox.io>
2025-10-02 02:37:36 +0000

    Sandbox snapshot commit

--- main.py
+++ main.py
@@ -2,7 +2,8 @@
 # How might you want to visualize playing hangman? 
 
 # Visualization
-hangman_stage = [
+def start_game():
+  hangman_stages = [
     """ 
       -----
       |   |
@@ -68,33 +69,44 @@ hangman_stage = [
     """
     
 ]
+  # Word choice
+  import random
+  word_list = ["python", "computer", "programming", "tennis", "television", "bicycle", "school", "mathematics", "videogame"]
+  chosen_word = random.choice(word_list)
 
-for i in hangman_stage:
-    print(i)
+  # Game setup
+  display = ["_"] * len(chosen_word)
+  lives = 6
+  guessed_letters = []
+  
+  print("Welcome to Hangman!")
+  print(hangman_stages[0])
 
-# Word choice
-import random
-word_list = ["python", "computer", "programming", "tennis", "television", "bicycle", "school", "mathematics", "videogame"]
-chosen_word = random.choice(word_list)
-
-# Game setup
-for i in chosen_word:
-    display += "_"
-    lives = 6
-    guessed_letters = []
-    
-# Guess letter 
-while lives > 0 and "_" in display:
+  while lives > 0 and "_" in display:
     guess = input("Guess a letter: ")
     if guess in chosen_word:
         print("There is a " + guess + "!")
+        if guess in guessed_letters:
+          print("You already guessed that letter.")
+          continue
+        guessed_letters += [guess]
         for i in range(len(chosen_word)):
             letter = chosen_word[i]
             if letter == guess:
                 display[i] = guess
     else:
         lives -= 1
+        print("Wrong guess! You have " + str(lives) + " remaining.")
         print(hangman_stages[len(hangman_stages) - 1 -lives])
+  
+  if "_" not in display:
+    print("Congratulations! You guessed the word!")
+  else:
+    print("Game Over! The word was: " + chosen_word)
+
+print(start_game())
+
+
 
 # How will you update the stages when you have words with different lengths and different unique letters? 
 # Don't change number of stages in hangman, but try to space out your stages. 
