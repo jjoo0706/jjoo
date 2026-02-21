a75a1cc042f0017d3bcf21aebb0673818b9a73e5
CodeSandbox <support@codesandbox.io>
2026-02-21 16:36:22 +0000

    Sandbox snapshot commit

--- hw260131.py
+++ hw260131.py
@@ -11,6 +11,16 @@ class Node:
         self.val = val
         self.next = next
 
+    def __str__(self):
+        result = ""
+        now = self
+        while now:
+            result += str(now.val)
+            if now.next:
+                result += " -> "
+            now = now.next
+        return result
+
 def Cycle(head):
     slow = head
     fast = head
@@ -22,17 +32,18 @@ def Cycle(head):
     return False
 
 
-# a = Node(1)
-# b = Node(2)
-# c = Node(3)
-# d = Node(4)
+a = Node(1)
+b = Node(2)
+c = Node(3)
+d = Node(4)
+e = Node(5)
 
-# a.next = b
-# b.next = c
-# c.next = d
-# d.next = b
+a.next = b
+b.next = c
+c.next = d
+d.next = e
 
-# print(Cycle(a))
+print(Cycle(a))
 
  # QUESTION 2
  # Write a function that will reorder nodes so that:
@@ -42,7 +53,6 @@ def Cycle(head):
  # Example:
  # Input: 1 -> 4 -> 3 -> 2 -> 5 -> 1, x = 3
  # Output: 1 -> 2 -> 1 -> 3 -> 4 -> 5
-
 
 def reorder(head, x):
     small = None
@@ -74,18 +84,23 @@ def reorder(head, x):
     else:
         return big
 
-a = Node(1)
-b = Node(4)
-c = Node(3)
-d = Node(2)
-e = Node(5)
-f = Node(1)
+values = [1, 4, 6, 2, 3, 5, 7]
 
-a.next = b
-b.next = c
-c.next = d
-d.next = e
-e.next = f
+head = None
+tail = None
 
-new_head = reorder(a, 3)
+for val in values:
+    new_node = Node(val)
+    if head is None:
+        head = new_node
+        tail = new_node
+    else:
+        tail.next = new_node
+        tail = new_node
+
+
+new_head = reorder(head, 3)
 print(new_head)
+
+# HW ASSIGNED 260221 - fix the bug on QUESTION 2 
+# git 
