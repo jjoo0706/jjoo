8267f7a94ee8c4108a76dac481f049a1dc0055b2
CodeSandbox <support@codesandbox.io>
2026-04-17 22:43:17 +0000

    Sandbox snapshot commit

--- binarysearch.py
+++ binarysearch.py
@@ -39,6 +39,24 @@ print(binary_search(nums, 7))
 # Exercise 1: In a list of repeating items, find the first instance of the item. Return index of first instance. 
 # [1,2,2,2,3], 2 -> 1 
 
+def first(lst, target):
+    left = 0
+    right = len(lst) - 1
+    result = -1
+    while left <= right:
+        mid = (left + right) // 2
+        if lst[mid] == target:
+            result = mid
+            right = mid - 1
+        elif lst[mid] < target:
+            left = mid + 1
+        else:
+            right = mid - 1
+    return result
+
+nums = [1, 3, 3, 4, 7, 8, 11, 15]
+print(first(nums, 3))
+
 # Exercise 2: In a list of repeating items, find the last instance of the item. Return index of last instance. 
 # [1,2,2,2,3], 2 -> 3 
 
